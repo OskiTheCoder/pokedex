292bfcccad2cfdad6da99d59ba81d2aa157a5b75
Shelvin Singh <shelvinsingh@Shelvins-MacBook-Pro-2.local>
2020-02-08 19:10:44 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Pokecard from "./Pokecard";
+
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+        <Pokecard id = {4} name = "Charmander" type = "fire" exp = {62}/>
+      </div>
+    )
+  }
+}
+
+export default App;
